fef185a15ab991691806fc441ccaa533263fbb2a
Richard Decal <public@richarddecal.com>
2024-03-29 22:52:25 -0400

    refactor code to support new tasks
    Signed-off-by: Richard Decal <public@richarddecal.com>

--- src/obsidian_llm/__main__.py
+++ src/obsidian_llm/__main__.py
@@ -6,13 +6,12 @@ import os
 import click
 from dotenv import load_dotenv
 
-from .alias_suggester import generate_alias_suggestions
-from .diff_generator import generate_diff_and_update
-from .frontmatter_verifier import verify_frontmatter
-from .markdown_enumerator import enumerate_markdown_files
+from obsidian_llm.alias_suggester import generate_all_aliases
 
 
-# Ensure environment variables are loaded
+logging.basicConfig(level=logging.INFO)
+
+# load configuration from .env file
 load_dotenv()
 
 
@@ -22,8 +21,14 @@ load_dotenv()
     type=click.Path(exists=True, file_okay=False, dir_okay=True, readable=True),
     required=False,
 )
+@click.option(
+    "--task",
+    "-t",
+    type=click.Choice(["aliases"]),
+    default="aliases",
+)
 @click.version_option()
-def main(vault_path) -> None:
+def main(vault_path, task) -> None:
     """Obsidian Vault Improvement Assistant."""
     if not vault_path:
         logging.info(
@@ -35,43 +40,12 @@ def main(vault_path) -> None:
                 "Vault path not provided. Please provide a valid path to the vault."
             )
             return
-    logging.basicConfig(level=logging.INFO)
-    md_files = enumerate_markdown_files(vault_path)
-    for file_path in md_files:
-        try:
-            content = ""
-            with open(file_path, encoding="utf-8") as file:
-                content = file.read()
-            frontmatter_dict, frontmatter_str = verify_frontmatter(file_path)
-            if frontmatter_dict:
-                if (
-                    "processed_for" in frontmatter_dict
-                    and "new_aliases" in frontmatter_dict["processed_for"]
-                ):
-                    logging.info(f"Skipping already processed file: {file_path}")
-                    continue
-                document_title = frontmatter_dict.get("title", "")
-                existing_aliases = frontmatter_dict.get("aliases", [])
-                new_aliases = generate_alias_suggestions(
-                    document_title, existing_aliases
-                )
-                if new_aliases:
-                    if "processed_for" not in frontmatter_dict:
-                        frontmatter_dict["processed_for"] = []
-                    frontmatter_dict["processed_for"].append("new_aliases")
-                    generate_diff_and_update(
-                        file_path, new_aliases, frontmatter_dict, content
-                    )
-                    logging.info(
-                        f"Diff generated and user decision processed for {file_path}."
-                    )
-                else:
-                    logging.info(f"No new aliases suggested for {file_path}. Skipping.")
-            else:
-                continue
-        except Exception as e:
-            logging.error(f"An error occurred while processing file {file_path}: {e}")
-            logging.error("Error trace:", exc_info=True)
+
+    if task == "aliases":
+        generate_all_aliases(vault_path)
+    else:
+        logging.error(f"Invalid task: {task}. Please provide a valid task.")
+        return
 
 
 if __name__ == "__main__":
